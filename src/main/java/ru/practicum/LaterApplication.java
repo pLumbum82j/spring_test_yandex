2c28f36bd8437f10212e9e5c73a05f1a728331f4
pLumbum82 <jobbsy@yandex.ru>
2023-03-29 17:40:55 +0500

    first commit

---
+++ src/main/java/ru/practicum/LaterApplication.java
@@ -0,0 +1,36 @@
+package ru.practicum;
+
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+public class LaterApplication {
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.getConnector().setPort(PORT);
+
+        // то самое "приложение" или "контекст" с пустым путём
+        Context tomcatContext = tomcat.addContext("", null);
+
+        AnnotationConfigWebApplicationContext applicationContext =
+                new AnnotationConfigWebApplicationContext();
+        applicationContext.setServletContext(tomcatContext.getServletContext());
+        applicationContext.scan("ru.practicum");
+        applicationContext.refresh();
+
+
+        // класс Wrapper позволяет задать дополнительные настройки для сервлета
+        Wrapper testServletWrapper =
+                Tomcat.addServlet(tomcatContext, "testServlet", new TestServlet());
+
+        // addMapping() сопоставляет URL-путь с сервлетом
+        testServletWrapper.addMapping("/test");
+
+        tomcat.start();
+    }
+}
